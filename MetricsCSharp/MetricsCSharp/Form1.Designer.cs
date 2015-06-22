80fa4640ebfcc7f1c34732110de8441d16b3f7d3
AKhloptsev <AKhloptsev@mail.ua>
2015-06-23 02:22:29 +0300

    Transfer of centimeters and inches using regular expression, a difference between verification and validation

---
+++ MetricsCSharp/MetricsCSharp/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MetricsCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxInputString = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxInputString
+            // 
+            this.textBoxInputString.Location = new System.Drawing.Point(67, 45);
+            this.textBoxInputString.Name = "textBoxInputString";
+            this.textBoxInputString.Size = new System.Drawing.Size(170, 20);
+            this.textBoxInputString.TabIndex = 0;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(67, 129);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(170, 20);
+            this.textBoxResult.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(64, 18);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(71, 13);
+            this.labelInput.TabIndex = 3;
+            this.labelInput.Text = "Input string ->";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 184);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxInputString);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Metrics";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInputString;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ MetricsCSharp/MetricsCSharp/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace MetricsCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Program to convert inchs and centimeters
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = "";
+            str = textBoxInputString.Text;
+            // regular expression
+            string re = "(([0-9]+[.,]*[0-9]*)|([0-9]*[.,]*[0-9]+)) ((cm)|(in))";
+            if (str != Regex.Match(str, re).ToString())
+            {
+                textBoxResult.Text = "Verification Error";
+                errorProvider1.SetError(textBoxInputString, "Wrong format");
+            }
+            else
+            {
+                float number = 0;  // for
+                string[] s = str.Split('.');
+                // change '.' to ','
+                str = s[0] + ',' + s[1];
+                try
+                {
+                    number = Convert.ToSingle(str.Substring(0, str.Length - 3));    // number = str - 3 symbols (" cm|in")
+                    if (number < 0f)
+                    {
+                        textBoxResult.Text = "Validation Error";
+                        errorProvider1.SetError(textBoxInputString, "Negative number");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    textBoxResult.Text = ex.Message;
+                }
+
+                float result = 0f;
+                bool bFlag = false;
+                if (str.Substring(str.Length - 1, 1) == "m")    // if last symbol = 'm' ("cm")
+                {
+                    result = number / 2.54f;
+                    bFlag = true;
+                }
+                // else ("in")
+                else
+                {
+                    result = number * 2.54f;
+                    bFlag = false;
+                }
+                if (bFlag)
+                    textBoxResult.Text = result.ToString() + " in";
+                else
+                    textBoxResult.Text = result.ToString() + " cm";
+            }
+        }
+    }
+}
