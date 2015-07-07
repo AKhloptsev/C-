366ab6b86fdf79a2064cc5fda2c305352894e5af
AKhloptsev <AKhloptsev@mail.ua>
2015-07-07 14:43:50 +0300

    Testing of regular expression

---
+++ MyRegex/MyRegex/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MyRegex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxReg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxReg
+            // 
+            this.textBoxReg.Location = new System.Drawing.Point(25, 28);
+            this.textBoxReg.Name = "textBoxReg";
+            this.textBoxReg.Size = new System.Drawing.Size(208, 20);
+            this.textBoxReg.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input regular expression:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input string:";
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Location = new System.Drawing.Point(28, 99);
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(202, 20);
+            this.textBoxText.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Result:";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(29, 199);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(199, 20);
+            this.textBoxResult.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 26);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 233);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxReg);
+            this.Name = "Form1";
+            this.Text = "Check Regular Expressions";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxReg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+    }
+}
+
---
+++ MyRegex/MyRegex/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace MyRegex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string regEx = this.textBoxReg.Text;    // regEx = regular expression
+            string text = this.textBoxText.Text;
+            bool bFlag = false;
+            if (regEx == "")
+            {
+                errorProvider1.SetError(textBoxReg, "Regular expression is absent");
+                this.textBoxResult.Text = "Regular expression is absent";
+                bFlag = true;
+            }
+            if (text == "")
+            {
+                errorProvider2.SetError(textBoxText, "Text is absent");
+                this.textBoxResult.Text = "Text is absent";
+                bFlag = true;
+            }
+
+            var rm = Regex.Match(text, regEx);
+
+            if (!bFlag)     // if there are no problems with validation
+            {
+                if (rm.ToString() == text)
+                    this.textBoxResult.Text = "Is regular expression";
+                else
+                    this.textBoxResult.Text = "Isn't regular expression";
+            }
+        }
+    }
+}
